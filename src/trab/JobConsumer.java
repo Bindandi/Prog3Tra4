b9728ab83f24a5834682ed60156d92e352fa7607
JÚLIO BINDANDI <juliocesarbindandi@gmail.com>
2020-11-23 00:00:16 -0300

    up

---
+++ src/trab/JobConsumer.java
@@ -0,0 +1,47 @@
+package trab;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class JobConsumer extends Thread {
+    private JobQueue jobs;
+    private Integer assignedJob = null;
+    private AtomicBoolean isRunning = new AtomicBoolean(true);
+
+    public JobConsumer(JobQueue jobs) {
+        this.jobs = jobs;
+    }
+
+    @Override
+    public void run() {
+        while (isRunning.get()) {
+            if (assignedJob == null || assignedJob == 0) {
+                try {
+                    assignedJob = jobs.getNextJob();
+                    if (assignedJob == null) {
+                        System.out.println("Nada para fazer zzz " + System.currentTimeMillis() + " " + this);
+                        this.sleep(5000);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                int workToDo = assignedJob;
+                for (int i = assignedJob; i >= 0; i--) {
+                    System.out.println("Trabalhando... Tamanho do Job " + assignedJob + ", " + System.currentTimeMillis() + ": " + this);                    
+                    try {
+                        this.sleep(1000);
+                    } catch (InterruptedException err) {
+                        err.printStackTrace();
+                    }
+                }
+                jobs.concludeJob();
+                assignedJob = null;
+            }
+            //testando...
+        }
+    }
+    
+    public void stopConsumer() {
+        isRunning.set(false);
+    }
+
+}
---
+++ src/trab/JobProducer.java
@@ -0,0 +1,26 @@
+package trab;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class JobProducer extends Thread {
+    private final JobQueue jobs;
+    private AtomicBoolean isRunning = new AtomicBoolean(true);
+
+    public JobProducer(JobQueue jobs) {
+        this.jobs = jobs;
+    }
+
+    @Override
+    public void run() {
+            try {
+                System.out.println("Adicionando uma atividade, " + this );
+                this.jobs.queueJob(1);
+            } catch (Exception err) {
+                System.out.println("Thread interoompida, " + this);
+            }
+        System.out.println("parando a thread, " + this);
+        this.interrupt();
+    }
+
+}
